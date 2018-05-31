754ef47ea6b24bf6381ce6e2acd3ecf2dc5c1881
Menno <302578878@student.rocmondriaan.nl>
2018-05-31 11:31:25 +0200

    Added login redirect

---
+++ src/AppBundle/Handler/LoginSuccessHandler.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Menno
+ * Date: 31-5-2018
+ * Time: 11:24
+ */
+
+namespace AppBundle\Handler;
+
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+
+    /** @var RouterInterface  */
+    private $router;
+
+    /** @var AuthorizationChecker  */
+    private $authorizationChecker;
+
+    public function __construct(RouterInterface $router, AuthorizationChecker $authorizationChecker)
+    {
+        $this->router = $router;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+        if($this->authorizationChecker->isGranted("ROLE_INSTRUCTOR")){
+            return new RedirectResponse($this->router->generate("adminHome"));
+        }
+
+//        if($this->authorizationChecker->isGranted("ROLE_MEMBER")){
+//            return new RedirectResponse($this->router->generate("memberHome"));
+//        }
+        return new RedirectResponse($this->router->generate("homepage"));
+
+    }
+}
